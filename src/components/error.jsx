8454c2de85f41361b97167f48a6d3f2e9c093142
MostafaMosaad <101674967+MostafaMosaad@users.noreply.github.com>
2023-03-21 13:27:51 +0200

    Delete src directory

--- src/components/error.jsx
+++
@@ -1,8 +0,0 @@
-function ErrorMe() {
-    return ( 
-    <>
-      <img src={process.env.PUBLIC_URL + "/Imges/Error.png"}  style={{  width:"60%", height:"20%",paddingTop:"5rem"}}/>
-    </> );
-}
-
-export default ErrorMe;
